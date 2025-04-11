3b8d6f1c4a020fe2dc495c392f074652674200e6
mobinasri <masri@ucsc.edu>
2025-04-11 13:27:01 -0700

    added tables and scripts for running haplotype sampling on graph-v2.0

---
+++ DeepVariant_Pangenome/haplotype_sampling_dv_prod_bam_files/copy_to_gcp.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=gcp_copy
+#SBATCH --partition=long
+#SBATCH --mail-user=masri@ucsc.edu
+#SBATCH --nodes=1
+#SBATCH --mem=8gb
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --output=%x.%j.log
+#SBATCH --time=72:00:00
+
+pwd; hostname; date
+
+set -e
+
+
+DIR="/private/groups/patenlab/masri/haplotype_sampling/dv_prod_bam_files/non_diploid"
+for i in $(find ${DIR} | grep ".gbz$");do echo $i; gsutil cp $i gs://pepper-deepvariant/mobinasri/haplotype_sampling/dv_prod_bam_files/non_diploid_sampling/ ;done
